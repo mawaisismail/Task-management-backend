c252dd5bf12df92c10d40424d8b31ce7ee3ebde0
talk2awais <muhammad.awais.77831@gmail.com>
2022-11-04 20:30:55 +0500

    Added getTaskWithCondition

--- src/task/tasks.controller.ts
+++ src/task/tasks.controller.ts
@@ -6,16 +6,21 @@ import {
   Param,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
-import { CreateTaskDto } from '../dto/create-task.dto';
+import { CreateTaskDto, TaskWithCondition } from '../dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
   @Get()
-  getAllTasks(): Task[] {
+  getAllTasks(@Query() taskWithCondition: TaskWithCondition): Task[] {
+    const { status, search } = taskWithCondition;
+    if (status || search) {
+      return this.tasksService.getTasksWithCondition(taskWithCondition);
+    }
     return this.tasksService.getAllTask();
   }
   @Post()
